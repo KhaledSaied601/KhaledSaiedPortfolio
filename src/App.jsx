2c28d826842bc010b79e7e730c759be5fa33cda5
KhaledSaied601 <ksaed110@gmail.com>
2024-09-24 17:22:26 +0300

    Fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LayOut from './components/LayOut/LayOut'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
