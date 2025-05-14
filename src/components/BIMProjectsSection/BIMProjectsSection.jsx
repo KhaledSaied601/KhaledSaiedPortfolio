bab74c72628018b9e9f4b3d8d14f6238fa74ecaf
Khaled Saied <ksaed110@gmail.com>
2025-05-14 14:56:28 +0300

    Fix CSS module import path

--- src/components/BIMProjectsSection/BIMProjectsSection.jsx
+++ src/components/BIMProjectsSection/BIMProjectsSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import Style from './BimProjectsSection.module.css'
+import Style from './BIMProjectsSection.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
@@ -130,40 +130,6 @@ function BimProjectsSection() {
                                     Find in-depth information about my Code on Navisworks Add-In.
                                 </p>
                             </Link>
-
-
-                            {/* <a
-                            href=""
-                            className="bg-clip-padding bg-gradient-to-b  from-white to-black  w-full h-full min-h-40 group rounded-lg border border-transparent px-5 py-4   bg-cover bg-center transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 overflow-hidden"
-
-                        >
-                            <div className='flex flex-row gap-2 items-center mb-3 lg:justify-between'>
-
-                                <h2 className=" text-xl font-bold">REVIT API</h2>
-                                <FontAwesomeIcon className=' w-5  transition-transform group-hover:translate-x-1 motion-reduce:transform-none' icon={faArrowRight} />
-                            </div>
-
-                            <p className="m-0 max-w-[30ch] text-sm font-semibold">
-                                Find in-depth information about my Code on Revit API.
-                            </p>
-                        </a>
-
-
-                        <a
-                            href=""
-                            className="bg-clip-padding bg-gradient-to-tr  from-white via-blue-500 to-black  w-full h-full min-h-40 group rounded-lg border border-transparent px-5 py-4   bg-cover bg-center transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 overflow-hidden"
-
-                        >
-                            <div className='flex flex-row gap-2 items-center mb-3 lg:justify-between'>
-
-                                <h2 className=" text-xl font-bold">TEKLA API</h2>
-                                <FontAwesomeIcon className=' w-5  transition-transform group-hover:translate-x-1 motion-reduce:transform-none' icon={faArrowRight} />
-                            </div>
-
-                            <p className="m-0 max-w-[30ch] text-sm font-semibold">
-                                Find in-depth information about my Code on Tekla API.
-                            </p>
-                        </a> */}
 
 
 
