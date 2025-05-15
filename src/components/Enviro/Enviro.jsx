2d8ccee2bf72037980df14038cb7e808ec45ffb7
Khaled Saied <ksaed110@gmail.com>
2025-05-15 11:39:36 +0300

    Update Enviro.jsx

--- src/components/Enviro/Enviro.jsx
+++ src/components/Enviro/Enviro.jsx
@@ -69,7 +69,7 @@ function Enviro() {
                     maxWidth={4}
                     textAlign="center"
                 >
-                    Khaled Saied
+                    Khaled Saieds
 
 
                 </Text>
